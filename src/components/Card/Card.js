ff9318b4d81eaf709933e3a7ec3ae451efbf102e
justinma98 <justinma3@gmail.com>
2019-08-02 15:31:44 -0700

    styled cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,21 +5,26 @@ import Rating from "react-rating";
 export default class Card extends Component {
   handleRating = rating => {
     console.log(`item ${this.props.id} got a rating of ${rating}`);
-  }
+  };
   render() {
     const props = this.props;
     return (
       <div id={props.id} className="card">
-        <img className="card__picture" src={props.image} />
-        <div className="card_info">
-          <span className="card__title">{props.title}</span>
+        <div className="card__main">
+          <img className="card__picture" src={props.image} />
+          <div className="card__fade" />
+          <div className="card__info">
+            <span className="card__title">{props.title}</span>
             <Rating
+              className="rating"
               initialRating={props.rating}
               readonly
               emptySymbol={<div className="rating__star -empty" />}
               fullSymbol={<div className="rating__star -full" />}
             />
+            <span>{`(${props.count})`}</span>
           </div>
+        </div>
         <div className="card__rating">
           <div className="userRating">
             <Rating
--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -6,30 +6,91 @@ const cards = [
   {
     id: 1,
     rating: 3,
+    count: 12,
     title: "Tate's Bake Shop Cookies",
-    image: "https://d2lnr5mha7bycj.cloudfront.net/product-image/file/large_5b0def3e-306b-4cf8-9bf8-9eaab1a5a52d.jpg"
-  },{
+    image:
+      "https://d2lnr5mha7bycj.cloudfront.net/product-image/file/large_5b0def3e-306b-4cf8-9bf8-9eaab1a5a52d.jpg"
+  },
+  {
     id: 2,
     rating: 3.5,
-    title: "Tate's Bake Shop Cookies",
-    image: "https://d2lnr5mha7bycj.cloudfront.net/product-image/file/large_5b0def3e-306b-4cf8-9bf8-9eaab1a5a52d.jpg"
-  },{
+    count: 2,
+    title: "Tate's Mini Cookies",
+    image:
+      "https://d2lnr5mha7bycj.cloudfront.net/product-image/file/large_5b0def3e-306b-4cf8-9bf8-9eaab1a5a52d.jpg"
+  },
+  {
     id: 3,
     rating: 4,
-    title: "Tate's Bake Shop Cookies",
-    image: "https://d2lnr5mha7bycj.cloudfront.net/product-image/file/large_5b0def3e-306b-4cf8-9bf8-9eaab1a5a52d.jpg"
-  },{
+    count: 129,
+    title: "Bake Shop Cookies",
+    image:
+      "https://d2lnr5mha7bycj.cloudfront.net/product-image/file/large_5b0def3e-306b-4cf8-9bf8-9eaab1a5a52d.jpg"
+  },
+  {
     id: 4,
     rating: 2.2,
-    title: "Tate's Bake Shop Cookies",
-    image: "https://d2lnr5mha7bycj.cloudfront.net/product-image/file/large_5b0def3e-306b-4cf8-9bf8-9eaab1a5a52d.jpg"
+    count: 4,
+    title: "Cookies",
+    image:
+      "https://d2lnr5mha7bycj.cloudfront.net/product-image/file/large_5b0def3e-306b-4cf8-9bf8-9eaab1a5a52d.jpg"
+  },
+  {
+    id: 5,
+    rating: 3.5,
+    count: 2,
+    title: "Tate's Mini Cookies",
+    image:
+      "https://d2lnr5mha7bycj.cloudfront.net/product-image/file/large_5b0def3e-306b-4cf8-9bf8-9eaab1a5a52d.jpg"
+  },
+  {
+    id: 6,
+    rating: 4,
+    count: 129,
+    title: "Bake Shop Cookies",
+    image:
+      "https://d2lnr5mha7bycj.cloudfront.net/product-image/file/large_5b0def3e-306b-4cf8-9bf8-9eaab1a5a52d.jpg"
+  },
+  {
+    id: 7,
+    rating: 2.2,
+    count: 4,
+    title: "Cookies",
+    image:
+      "https://d2lnr5mha7bycj.cloudfront.net/product-image/file/large_5b0def3e-306b-4cf8-9bf8-9eaab1a5a52d.jpg"
+  },
+  {
+    id: 8,
+    rating: 3.5,
+    count: 2,
+    title: "Tate's Mini Cookies",
+    image:
+      "https://d2lnr5mha7bycj.cloudfront.net/product-image/file/large_5b0def3e-306b-4cf8-9bf8-9eaab1a5a52d.jpg"
+  },
+  {
+    id: 9,
+    rating: 4,
+    count: 129,
+    title: "Bake Shop Cookies",
+    image:
+      "https://d2lnr5mha7bycj.cloudfront.net/product-image/file/large_5b0def3e-306b-4cf8-9bf8-9eaab1a5a52d.jpg"
+  },
+  {
+    id: 10,
+    rating: 2.2,
+    count: 4,
+    title: "Cookies",
+    image:
+      "https://d2lnr5mha7bycj.cloudfront.net/product-image/file/large_5b0def3e-306b-4cf8-9bf8-9eaab1a5a52d.jpg"
   }
 ];
 
 class Landing extends Component {
   render() {
     return (
-      <div className="landing">
+      <div>
+        <div className="background" />
+        <div className="header">Vote for your favorite snacks...</div>
         <CardContainer cards={cards} />
       </div>
     );
