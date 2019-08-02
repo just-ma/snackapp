d1ce3d3e965a19b4ec5f0c14981a123e52c2e574
Justin Ma <justinma3@gmail.com>
2019-08-02 16:28:53 -0700

    sort by

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -86,12 +86,65 @@ const cards = [
 ];
 
 class Landing extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sort: "none"
+    };
+  }
+
+  sortArr = arr => {
+    return arr.sort((a, b) => {
+      switch (this.state.sort) {
+        case "popularity":
+          return b.count - a.count;
+        case "highrating":
+          return b.rating - a.rating;
+        case "lowrating":
+          return a.rating - b.rating;
+        default:
+          return 1;
+      }
+    });
+  };
+
   render() {
     return (
-      <div>
+      <div className="landing">
         <div className="background" />
-        <div className="header">Vote for your favorite snacks...</div>
-        <CardContainer cards={cards} />
+        <div className="landing__header">
+          <div className="landing__title">Vote for your favorite snacks...</div>
+          <div className="landing__sort">
+            <div>Sort by:</div>
+            <span
+              style={{
+                fontWeight: this.state.sort === "popularity" ? 600 : null
+              }}
+              onClick={() => this.setState({ sort: "popularity" })}
+            >
+              Popularity
+            </span>
+            <span>&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;</span>
+            <span
+              style={{
+                fontWeight: this.state.sort === "highrating" ? 600 : null
+              }}
+              onClick={() => this.setState({ sort: "highrating" })}
+            >
+              Highest Rated
+            </span>
+            <span>&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;</span>
+            <span
+              style={{
+                fontWeight: this.state.sort === "lowrating" ? 600 : null
+              }}
+              onClick={() => this.setState({ sort: "lowrating" })}
+            >
+              Lowest Rated
+            </span>
+          </div>
+        </div>
+        <CardContainer cards={this.sortArr(cards)} />
       </div>
     );
   }
